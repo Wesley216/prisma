21ff74acb5c40bfe15f84efe83cf77575c4ae660
kali <wbarbosa@outlook.com>
2022-10-14 19:18:49 -0300

    commit backup

---
+++ prima-nest/src/modules/book/book.service.ts
@@ -0,0 +1,74 @@
+import { Injectable } from '@nestjs/common';
+import { async } from 'rxjs';
+import { PrismaService } from 'src/database/PrismaService';
+import { BookDTO } from './book.dto';
+
+@Injectable()
+export class BookService {
+    constructor(private prisma: PrismaService) {
+
+    }
+
+    async create(data: BookDTO) {
+
+        const bookExists = await this.prisma.book.findFirst({
+            where: {
+                bar_code: data.bar_code
+            }
+        })
+
+        if (bookExists) {
+            throw new Error("Error already exists");
+
+        }
+
+        const book = await this.prisma.book.create({
+            data
+        });
+
+        return book;
+    }
+
+    async findAll() {
+        return this.prisma.book.findMany();
+    }
+
+    async update(id: string, data: BookDTO) {
+        const bookExists = await this.prisma.book.findUnique({
+            where: {
+                id,
+            },
+        });
+
+        if (!bookExists) {
+            throw new Error("Book does not exists");
+
+        }
+
+        return await this.prisma.book.update({
+            data,
+            where: {
+                id,
+            },
+        });
+    }
+
+    async delete(id: string) {
+        const bookExists = await this.prisma.book.findUnique({
+            where: {
+                id,
+            },
+        });
+
+        if (!bookExists) {
+            throw new Error("Book does not exists");
+
+        }
+
+        return await this.prisma.book.delete({
+            where:{
+                id,
+            }
+        });
+    }
+}
